9e0fbb13450092aae122a9c0a6f86bcce7f66ff3
Carmine Di Gruttola <carmine.di.gruttola@pwc.com>
2023-09-02 09:57:07 +0200

    modify sh

--- zip-for-marketplace.sh
+++ zip-for-marketplace.sh
@@ -1,18 +1,20 @@
+#!/bin/sh
+
+BASENAME=$(basename "$PWD")
+
 rm -rf tmp
-mkdir -p tmp/bocustomize
-cp -R docs tmp/bocustomize
-cp -R config tmp/bocustomize
-cp -R override tmp/bocustomize
-cp -R translations tmp/bocustomize
-cp -R views tmp/bocustomize
-cp -R upgrade tmp/bocustomize
-cp -R vendor tmp/bocustomize
-cp -R index.php tmp/bocustomize
-cp -R logo.png tmp/bocustomize
-cp -R bocustomize.php tmp/bocustomize
-cp -R config.xml tmp/bocustomize
-cp -R LICENSE tmp/bocustomize
-cp -R README.md tmp/bocustomize
-cp -R composer.json tmp/bocustomize
+mkdir -p tmp/"$BASENAME"
+cp -R config tmp/"$BASENAME"
+cp -R controllers tmp/"$BASENAME"
+cp -R override tmp/"$BASENAME"
+cp -R translations tmp/"$BASENAME"
+cp -R views tmp/"$BASENAME"
+cp -R upgrade tmp/"$BASENAME"
+cp -R vendor tmp/"$BASENAME"
+cp -R index.php tmp/"$BASENAME"
+cp -R "$BASENAME".php tmp/"$BASENAME"
+cp -R config.xml tmp/"$BASENAME"
+cp -R LICENSE tmp/"$BASENAME"
+cp -R README.md tmp/"$BASENAME"
 cd tmp && find . -name ".DS_Store" -delete
-zip -r bocustomize.zip . -x ".*" -x "__MACOSX"
+zip -r "$BASENAME".zip . -x ".*" -x "__MACOSX"
